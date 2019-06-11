c8b7b9939e5be14c927d2fe1d658239c93d2d666
wangwangla <2818815189@qq.com>
2019-06-11 14:21:10 +0800

    页面底部布局书写

--- MobilePlayer/app/src/main/java/test/kw/mobileplayer/Maintivity.java
+++ MobilePlayer/app/src/main/java/test/kw/mobileplayer/Maintivity.java
@@ -4,18 +4,22 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class Maintivity extends Activity {
+    //实例化 帧布局
+    private FrameLayout fl_main_content;
+    //下栏
+    private RadioGroup rg_buttom_tag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(this);
-        textView.setText("主页面");
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        setContentView(textView);
-
+        setContentView(R.layout.activity_main);
+        fl_main_content = findViewById(R.id.fl_main_content);
+        rg_buttom_tag = findViewById(R.id.rg_buttom_tag);
+        //默认选中
+        rg_buttom_tag.check(R.id.rb_video);
     }
 }
