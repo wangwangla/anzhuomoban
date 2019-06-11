aaf1ffa598f626e34d006d1fd35e3c7c9bf10323
wangwangla <2818815189@qq.com>
2019-06-11 14:27:34 +0800

    BasePager书写

---
+++ MobilePlayer/app/src/main/java/test/kw/mobileplayer/base/BasePager.java
@@ -0,0 +1,31 @@
+package test.kw.mobileplayer.base;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * 基类
+ */
+public abstract class BasePager {
+    //构造方法将试图创建  孩子强制继承
+    //初始化子页面的数据
+    private Context context;
+
+    private View rootview;
+    public BasePager(Context context){
+        this.context = context;
+        rootview = initView();
+    }
+
+    /**
+     * 强制实现
+     * @return
+     */
+    public abstract View initView();
+
+    /**
+     * 绑定数据的时候，重写方法
+     */
+    public void initData(){}
+
+}
