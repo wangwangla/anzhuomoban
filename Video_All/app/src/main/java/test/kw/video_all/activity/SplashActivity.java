b62693efcdce4224c36d9f929027388a8e4d5216
wangwangla <2818815189@qq.com>
2019-06-16 10:33:29 +0800

    点击屏幕，执行跳转，处理重复启动 问题

--- Video_All/app/src/main/java/test/kw/video_all/activity/SplashActivity.java
+++ Video_All/app/src/main/java/test/kw/video_all/activity/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import test.kw.video_all.R;
 
@@ -23,8 +24,22 @@ public class SplashActivity extends Activity {
     }
 
     private void startMainActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        if (!isStartMain) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+    private boolean isStartMain = false;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //触摸屏幕执行跳转，也可以通过点击按钮执行跳转。
+        //问题：触摸的时候执行一次，在定时到在执行一次，所以可以加判断
+        /**
+         * 处理方法一：加入一个变量创建执行将其状态改变
+         * 处理方式二:使用单例
+         */
+        startMainActivity();
+        return super.onTouchEvent(event);
     }
 }
